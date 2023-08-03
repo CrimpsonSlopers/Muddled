245c5e2be3c7d5e1930610de0d2109f9c869ff0c
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2023-08-02 20:34:28 -0500

    Update urls.py

--- muddle/urls.py
+++ muddle/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path('', include("frontend.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
