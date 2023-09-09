a5bbcef89f79dc96579e559a22cac6f897a6949a
John Rogers <jrhawktwo@gmail.com>
2023-09-09 17:26:53 -0500

    update

--- frontend/src/pages/Landing/index.js
+++ frontend/src/pages/Landing/index.js
@@ -26,19 +26,6 @@ export default function Landing() {
                     <Typography variant='body1' component='div' sx={{ marginRight: '20px', fontWeight: "bold" }}>
                         <a href='/get-smarter' style={{ color: 'white', textDecoration: 'none' }}>Get Smarter</a>
                     </Typography>
-<<<<<<< HEAD
-                    {/*
-                    <Typography variant='body1' component='div' sx={{ marginRight: '20px', fontWeight: "bold" }}>
-                        <a
-                            href={'/api/twitch_oauth_login/'}
-                            style={{ color: 'white', textDecoration: 'none', fontWeight: 'light' }}
-                        >
-                            Sign In
-                        </a>
-                    </Typography>
-                    */}
-=======
->>>>>>> d4905a1df46ec4be3f841deee86225f88f6aa5fe
                 </Toolbar>
             </AppBar>
         </Box>
