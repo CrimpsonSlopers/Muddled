51a0e4fcdbeda12582bbfa264d61363492060b37
John Rogers <jrhawktwo@gmail.com>
2023-09-09 17:44:49 -0500

    updates

--- frontend/src/pages/GetSmarter/components/YoutubeVideo.js
+++ frontend/src/pages/GetSmarter/components/YoutubeVideo.js
@@ -7,7 +7,7 @@ export default function YouTubeVideo({ video }) {
             width={'320px'}
             height={'180px'}
             src={`https://www.youtube.com/embed/${video.video_id}?&rel=0`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture; web-share"
             frameborder="0"
             allowfullscreen
         ></iframe>
